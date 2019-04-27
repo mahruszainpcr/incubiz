8e96260dcfd14b4f59fb3aa22611deaa5d4eeed0
Admin <admin@Admins-MacBook-Pro.local>
2019-04-27 09:03:19 +0700

    Updates

---
+++ precache-manifest.1c21e7407c7060df5d78cbb11468afd3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "721d117a4700f88e6b20253ae42a6809",
+    "url": "/incubiz/index.html"
+  },
+  {
+    "revision": "50f86729a4b7f6a5e87c",
+    "url": "/incubiz/static/css/main.56cdbaca.chunk.css"
+  },
+  {
+    "revision": "72f2709c2e0407729a61",
+    "url": "/incubiz/static/js/2.0d8f3303.chunk.js"
+  },
+  {
+    "revision": "50f86729a4b7f6a5e87c",
+    "url": "/incubiz/static/js/main.53b7d0b7.chunk.js"
+  },
+  {
+    "revision": "d4d95a0412f554c26d2c",
+    "url": "/incubiz/static/js/runtime~main.909fb7b0.js"
+  }
+]);
